eb9fabd701863371e5b7cc6ecb379018ffee1413
Ken Bonny <bonny.ken@gmail.com>
2017-06-05 13:43:15 +0200

    Add implementation that fetches weather data from csv file.

--- KenBonny.CodeKata04.DataAccess/Weather/CsvWeatherRepository.cs
+++ KenBonny.CodeKata04.DataAccess/Weather/CsvWeatherRepository.cs
@@ -1,4 +1,7 @@
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using CsvHelper;
 using KenBonny.CodeKata04.Console.Weather;
 
 namespace KenBonny.CodeKata04.DataAccess.Weather
@@ -14,7 +17,22 @@ namespace KenBonny.CodeKata04.DataAccess.Weather
         
         public IReadOnlyCollection<DayTemperature> LoadAllTemperatures()
         {
-            throw new System.NotImplementedException();
+            var dayTemperatures = new Collection<DayTemperature>();
+
+            using(TextReader weatherFile = File.OpenText(_fileLocation))
+            using (var weatherReader = new CsvReader(weatherFile))
+            {
+                while (weatherReader.Read())
+                {
+                    var day = weatherReader.GetField<int>(0);
+                    var maxTemp = weatherReader.GetField<int>(1);
+                    var minTemp = weatherReader.GetField<int>(2);
+                    var dayTemperature = new DayTemperature(day, maxTemp, minTemp);
+                    dayTemperatures.Add(dayTemperature);
+                }
+            }
+
+            return dayTemperatures;
         }
     }
 }
