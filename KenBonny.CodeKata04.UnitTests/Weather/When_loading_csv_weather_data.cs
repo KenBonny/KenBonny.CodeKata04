d2c1f53287fdcde20dceda9af5da1fc8c7561052
Ken Bonny <bonny.ken@gmail.com>
2017-06-05 17:25:49 +0200

    Update test to test Dispose.

--- KenBonny.CodeKata04.UnitTests/Weather/When_loading_csv_weather_data.cs
+++ KenBonny.CodeKata04.UnitTests/Weather/When_loading_csv_weather_data.cs
@@ -13,15 +13,16 @@ namespace KenBonny.CodeKata04.UnitTests.Weather
         public void Then_return_weather_data_from_memory()
         {
             DayTemperature[] dayTemperatures;
-
+            
             using (TextReader weatherReader = new StringReader(
                 @"Dy,MxT,MnT,AvT,HDDay,AvDP,1HrP TPcpn,WxType,PDir,AvSp,Dir,MxS,SkyC,MxR,Mn,R AvSLP
                     1,88,59,74,,53.8,0,F,280,9.6,270,17,1.6,93,23,1004.5
                     2,79,63,71,,46.5,0,,330,8.7,340,23,3.3,70,28,1004.5
                     3,77,55,66,,39.6,0,,350,5,350,9,2.8,59,24,1016.8"))
-            using (IWeatherRepository weatherRepository = new CsvWeatherRepository(weatherReader))
             {
+                IWeatherRepository weatherRepository = new CsvWeatherRepository(weatherReader);
                 dayTemperatures = weatherRepository.LoadAllTemperatures().ToArray();
+                weatherRepository.Dispose();
             }
             
             Assert.Equal(3, dayTemperatures.Length);
