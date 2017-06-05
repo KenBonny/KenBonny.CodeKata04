c603aef7d9c94a3b773da3ddc17f3b9632c3dd21
Ken Bonny <bonny.ken@gmail.com>
2017-06-05 16:24:16 +0200

    Add writer for smallest temperature spread.

---
+++ KenBonny.CodeKata04.Console/Weather/ISmallestTemperatureSpreadWriter.cs
@@ -0,0 +1,7 @@
+namespace KenBonny.CodeKata04.Console.Weather
+{
+    public interface ISmallestTemperatureSpreadWriter
+    {
+        void Write(SmallestTemperatureSpread smallestTemperatureSpread);
+    }
+}
